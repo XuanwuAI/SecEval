8e6b60bdfc808ee64eccd02c633139bb8c53cf26
ylxmf <ethanelift@gmail.com>
2023-12-17 18:49:24 +0800

    Update leaderboard

--- javascript/leaderboard/leaderboard.js
+++ javascript/leaderboard/leaderboard.js
@@ -1,20 +1,19 @@
 let obj_to_tr = (obj) => {
     return `<tr>
         <td>${obj["#"]}</td>
-        <td style="text-align: left;"><strong>${obj.Method}</strong></td>
-        <td><a href="${obj.Source.Link}" class="ext-link" style="font-size: 16px;">${obj.Source.Source}</a></td>
-        <td>${obj.Date}</td>
-        <td>${obj["#Size"]}</td>
-        <td>${obj["#Param"]}</td>
+        <td>${obj.Model}</td>
+        <td>${obj.Creator}</td>
+        <td>${obj.Access}</td>
+        <td>${obj["Submission Date"]}</td>
         <td>${obj.Avg}</td>
-        <td>${obj.NAT}</td>
-        <td>${obj.SOC}</td>
-        <td>${obj.LAN}</td>
-        <td>${obj.TXT}</td>
-        <td>${obj.IMG}</td>
-        <td>${obj.NO}</td>
-        <td>${obj["G1-6"]}</td>
-        <td>${obj["G7-12"]}</td>
+        <td>${obj["Network Security"]}</td>
+        <td>${obj.Vulnerability}</td>
+        <td>${obj["Memeory Security"]}</td>
+        <td>${obj["Web Security"]}</td>
+        <td>${obj["App Securiry"]}</td>
+        <td>${obj.Cryptography}</td>
+        <td>${obj["System Security"]}</td>
+        <td>${obj.Pentest}</td>
     </tr>`
 }
 
